875ac79b1db3083be44e5d5be182e3b9080133ae
daisy1754 <d-part@hotmail.co.jp>
2014-06-07 20:14:28 +0900

    Init demo project

---
+++ PhotoGalleryDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoGalleryDemo
+//
+//  Created by Kazuki Nishiura on 2014/06/07.
+//  Copyright (c) 2014年 Emaki inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
